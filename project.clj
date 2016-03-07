c1e09c4a50dbfffa035264d08ede3edecbaad189
Alexander Kiel <alexanderkiel@gmx.net>
2016-03-07 11:54:03 +0100

    Release Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lens-sds-batch "0.1.5"
+(defproject lens-sds-batch "0.1.6"
   :description "Lens Study Data Store Batch Processor"
   :url "https://github.com/alexanderkiel/lens-sds-batch"
   :license {:name "Eclipse Public License"
