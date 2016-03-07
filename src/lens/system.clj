bb8eba47da1240c42ac78f090c1553cb90500e70
Alexander Kiel <alexanderkiel@gmx.net>
2016-03-07 11:22:34 +0100

    Add Broker Port Config

--- src/lens/system.clj
+++ src/lens/system.clj
@@ -6,14 +6,15 @@
             [lens.util :as u]
             [lens.import-clinical-data]))
 
-(defnk new-system [lens-sds-batch-version port broker-host]
+(defnk new-system [lens-sds-batch-version port broker-host {broker-port "5672"}]
   (comp/system-map
     :version lens-sds-batch-version
     :port (u/parse-long port)
     :thread 1
 
     :broker
-    (new-broker {:host broker-host :num-batch-threads 1})
+    (new-broker {:host broker-host :port (u/parse-long broker-port)
+                 :num-batch-threads 1})
 
     :server
     (comp/using (new-server) [:port :thread :broker])))
